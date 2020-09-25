fbf08136e08c662d3eae443725e1bd700123e944
Matthew Weil <matt@weil.dev>
2020-09-25 11:21:38 -0400

    Update Slingshot.cs

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+	[Header("Set in Inspector")]
+	public GameObject prefabProjectile;
 	
-	public GameObject launchPoint;
-
+    [Header("Set Dynamically")]                                            
+    public GameObject launchPoint;
+    public Vector3 launchPos;                                   
+    public GameObject projectile;                             
+    public bool aimingMode;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +20,30 @@ public class Slingshot : MonoBehaviour
     }
 	
     void Awake() {
-
-        Transform launchPointTrans = transform.Find("LaunchPoint");        
+        Transform launchPointTrans = transform.FindChild("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;   
+
+    }
+	
+    void OnMouseDown() {                         
+
+        // The player has pressed the mouse button while over Slingshot
+
+        aimingMode = true;
+
+        // Instantiate a Projectile
+
+        projectile = Instantiate( prefabProjectile ) as GameObject;
+
+        // Start it at the launchPoint
+
+        projectile.transform.position = launchPos;
+
+        // Set it to isKinematic for now
+
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
 
     }
 	
