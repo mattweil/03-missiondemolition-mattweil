e541008fa384d747e51935cd1051fe4b7f46de88
Matthew Weil <matt@weil.dev>
2020-09-25 11:16:47 -0400

    add halo to slingshot

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+	
+	public GameObject launchPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+    void Awake() {
+
+        Transform launchPointTrans = transform.Find("LaunchPoint");        
+        launchPoint = launchPointTrans.gameObject;
+
+    }
+	
+    void OnMouseEnter() {
+		launchPoint.SetActive( true );
+        print("Slingshot:OnMouseEnter()");
+
+    }
+
+
+
+    void OnMouseExit() {
+
+        print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive( false );
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
